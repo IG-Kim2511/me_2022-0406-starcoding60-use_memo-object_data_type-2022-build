618eb812e5c3ad85e7e4a216ddff7059054825ea
IG(Ingyum) Kim <59094101+IG-Kim2511@users.noreply.github.com>
2022-04-19 11:24:58 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,527 @@
+/* 🍀 data detail - main projects */
+
+const Data_js = [
+    {
+        id: 0,
+        title: "Ecommerce-multiPage-SASS-Swiper-Toast",
+        skills: ["fa-js","fa-sass"],
+        explain: "fakestore_api, multipage, adding product to cart, calculate price, localstorage, rendering products from data, Swiper slideshow, Toast message, selecting featured images, Vanilla javascript",
+        imgSrc: "./img/projects/js-me_2022-0314-Ecommerce-multiPage-SASS-Swiper-Toast.jpg",
+        github: "https://github.com/IG-Kim2511/me_2022-0314-Ecommerce-multiPage-SASS-Swiper-Toast",
+        visit: "https://ig-kim2511.github.io/me_2022-0314-Ecommerce-multiPage-SASS-Swiper-Toast/",
+      
+        //🍖js13. numberOfUnits:1
+      },
+      {
+        id: 205,
+        type:"js-web",
+        title: "ecommerce_shoes_slideshow",
+    
+        skills: ["fa-js","fa-css3"],
+        explain: "adding product to cart, calculate price, localstorage, rendering products from data, slideshow, selecting featured images, Vanilla javascript",
+        imgSrc: "./img/projects/me_2021-1230-ecommerce_shoes_nike_shop.jpg",
+        github: "https://github.com/IG-Kim2511/me_2021-1230-Ecommerce_Shoes-Slideshow-JS",
+        visit:"https://ig-kim2511.github.io/me_2021-1230-Ecommerce_Shoes-Slideshow-JS/",
+      },
+  ];
+  
+
+
+const Data_react = [
+  {
+    id: 401,
+    title: "ecommerce_redux_fakestore_api",
+    skills: ["fa-react","fa-css3"],
+    explain: "redux, fakestore_api ",
+    imgSrc: "./img/projects/react-me_2022-0309-ecommerce_redux_fakestore_api-react-.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0309-ecommerce_redux_fakestore_api-react-",
+    visit:"https://ig-kim2511.github.io/me_2022-0309-ecommerce_redux_fakestore_api-react-build/",  
+  },
+  ];
+
+
+
+/* 🍀data sheets */
+  
+const Data_sheet_js_web = [
+  {
+    id: 200,
+    type:"js-web",
+    title: "3D-Wall-scrolling",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "offsetHeight, innerHeight,pageYOffset, e.target, e.clientX, resize...etc, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-0813_3D-Wall-scrolling.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021_0813-3d-Wall-js",
+    visit:"https://ig-kim2511.github.io/me_2021_0813-3d-Wall-js/",  
+  },  
+  {
+    id: 201,
+    type:"js-web",
+    title: "portfolio",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "rendering products from data, Vanilla javascript",
+    imgSrc: "./img/projects/me-portfolio-javascript.jpg",
+    github: "https://github.com/IG-Kim2511/Portfolio-me-2021-0712",
+    visit:"https://ig-kim2511.github.io/Portfolio-me-2021-0712/",  
+  },  
+  {
+    id: 202,
+    type:"js-web",
+    title: "Pokemon shopping cart",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "adding product to cart, calculate price, localstorage, rendering products from data, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-1101-Pokemon_Shopping_Cart-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-1101-Pokemon_Shopping_Cart-js",
+    visit:"https://ig-kim2511.github.io/me_2021-1101-Pokemon_Shopping_Cart-js/",  
+  },  
+
+  {
+    id: 204,
+    type:"js-web",
+    title: "Clothes Shopping Cart",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "adding product to cart, calculate price, localstorage, rendering products from data, selecting featured images, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-1109-cloth-Shopping_Cart.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-1109-cloth-Shopping_Cart-ig-js",
+    visit:"https://ig-kim2511.github.io/me_2021-1109-cloth-Shopping_Cart-ig-js/",
+  
+  },
+  {
+    id: 205,
+    type:"js-web",
+    title: "ecommerce_shoes_slideshow",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "adding product to cart, calculate price, localstorage, rendering products from data, slideshow, selecting featured images, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-1230-ecommerce_shoes_nike_shop.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-1230-Ecommerce_Shoes-Slideshow-JS",
+    visit:"https://ig-kim2511.github.io/me_2021-1230-Ecommerce_Shoes-Slideshow-JS/",
+  },
+  {
+    id: 0,
+    title: "Ecommerce-multiPage-SASS-Swiper-Toast",
+    skills: ["fa-js","fa-sass"],
+    explain: "fakestore_api, multipage, adding product to cart, calculate price, localstorage, rendering products from data, Swiper slideshow, Toast message, selecting featured images, Vanilla javascript",
+    imgSrc: "./img/projects/js-me_2022-0314-Ecommerce-multiPage-SASS-Swiper-Toast.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0314-Ecommerce-multiPage-SASS-Swiper-Toast",
+    visit: "https://ig-kim2511.github.io/me_2022-0314-Ecommerce-multiPage-SASS-Swiper-Toast/",
+  
+    //🍖js13. numberOfUnits:1
+  },
+];
+
+
+
+const Data_sheet_js_app = [
+  {
+    id: 300,
+    type:"js------",
+    title: "todolist",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "rendering from input value, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-0610_todolist-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0610_todolist-js",
+    visit:"https://ig-kim2511.github.io/me_2021-0610_todolist-js/",
+  },  
+
+  {
+    id: 301,
+    type:"js------",
+    title: "Expense Tracker",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "rendering from input value, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-0614_expense_tracker-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0614_expense_tracker-js",
+    visit:"https://ig-kim2511.github.io/me_2021-0614_expense_tracker-js/",
+  },  
+
+  {
+    id: 302,
+    type:"js-app",
+    title: "Note taking",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "rendering from input value, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-0621_note_taking-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0621_note_taking-js",
+    visit:"https://ig-kim2511.github.io/me_2021-0621_note_taking-js/",
+  },  
+  {
+    id: 303,
+    type:"js-app",
+    title: "covid_chart",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "fetch data, Chart.js, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-0422-covid_project-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0422-covid_project-js",
+    visit:"https://ig-kim2511.github.io/me_2021-0422-covid_project-js/",
+  },  
+  {
+    id: 304,
+    type:"js-app",
+    title: "Typing Game",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "fetch data api and play typing game, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-0411_typingGame.jpg",
+    github: "https://github.com/IG-Kim2511/c2021_0411-me_js_typingGame",
+    visit:"https://ig-kim2511.github.io/c2021_0411-me_js_typingGame/",
+  }, 
+];
+
+const Data_sheet_react = [
+  {
+    id: 400,
+    title: "portfolio",    
+    skills: ["fa-react","fa-sass"],
+    explain: "SASS, basic skills, Vanilla javascript",
+    imgSrc: "./img/projects/me-portfolio_with_React,SCSS.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0301_portfolio-react",
+    visit:"https://ig-kim2511.github.io/me_2021-0301_portfolio-react_build/",  
+  },
+
+  {
+    id: 401,
+    title: "search_filter",
+    skills: ["fa-react"],
+    explain: "fetch data api, filter, includes, some ",
+    imgSrc: "./img/projects/react-me_2022-0223-search_filter-react.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0223-search_filter-react",
+    visit:"https://ig-kim2511.github.io/me_2022-0223-search_filter-react-build/",  
+  },
+  {
+    id: 401,
+    title: "ecommerce_design-styled_components",
+    skills: ["fa-react"],
+    explain: "styled componets, slide ",
+    imgSrc: "./img/projects/react-me_2022-0302-ecommerce_design-styled_components-react.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0302-ecommerce_design-styled_components-react",
+    visit:"https://ig-kim2511.github.io/me_2022-0302-ecommerce_design-styled_components-react----build/",  
+  },
+  {
+    id: 401,
+    title: "ecommerce_redux_fakestore_api",
+    skills: ["fa-react","fa-css3"],
+    explain: "redux, fakestore_api ",
+    imgSrc: "./img/projects/react-me_2022-0309-ecommerce_redux_fakestore_api-react-.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0309-ecommerce_redux_fakestore_api-react-",
+    visit:"https://ig-kim2511.github.io/me_2022-0309-ecommerce_redux_fakestore_api-react-build/",  
+  },
+
+];
+
+
+const Data_sheet_small = [
+  {
+    id: 600,
+    type:" ",
+    title: " Virtual Keyboard for number",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "Virtual Keyboard for number",
+    imgSrc: "./img/projects/me_2021-1103-keyboard-number.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-1103_keyboard-js",
+    visit:"https://ig-kim2511.github.io/me_2021-1103_keyboard-js/",    
+  },  
+  {
+    id: 601,
+    type:" -- ",
+    title: "email.js",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "framwork emailjs",
+    imgSrc: "./img/projects/small-email.js.jpg",
+    github: "https://github.com/IG-Kim2511/email.js",
+    visit: "https://ig-kim2511.github.io/email.js/" ,    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "notification_ballon",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "--",
+    imgSrc: "./img/projects/notification-ballon.jpg",
+    github: "https://github.com/IG-Kim2511/notification_ballon",
+    visit:"https://ig-kim2511.github.io/notification_ballon/",    
+  },  
+  {
+    id: 613,
+    type:"Cat_chasing_ball",
+    title: "Cat_chasing_ball",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "e.clientX, e.clientY, event-transition",
+    imgSrc: "./img/projects/me_2021-0911-Cat_chasing_ball.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0911-Cat_chasing_ball300-js-folder",
+    visit:"https://ig-kim2511.github.io/me_2021-0911-Cat_chasing_ball300-js-folder/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "Shooting practice game",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "removeChild(), location.reload()",
+    imgSrc: "./img/projects/me-portfolio_shootingPractice-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021_0731-shootingPractice170-js",
+    visit:"https://ig-kim2511.github.io/me_2021_0731-shootingPractice170-js/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "door_ghost_game",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "--",
+    imgSrc: "./img/projects/me_2021-0909-door_ghost_game.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0909-door_ghost_game190-js-folder",
+    visit:"https://ig-kim2511.github.io/me_2021-0909-door_ghost_game190-js-folder",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "scroll_event",
+
+    skills: ["fa-js","fa-css3"],
+    explain: ".getBoundingClientRect().top;,.pageYOffset,.scrollY,.innerHeight,.offsetTop",
+    imgSrc: "./img/projects/me_2021-0910-scroll_event.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0910-scroll_event280-js-folder",
+    visit:"https://ig-kim2511.github.io/me_2021-0910-scroll_event280-js-folder/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "stoping pokecoin wave",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "setInterval, clearInterval",
+    imgSrc: "./img/projects/me_2021-0912-setInterval_pokecoin_wave.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0912-setInterval_pokecoin_wave320-js-folder",
+    visit:"https://ig-kim2511.github.io/me_2021-0912-setInterval_pokecoin_wave320-js-folder/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "lucky",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "requestAnimationFrame, cancelAnimationFrame",
+    imgSrc: "./img/projects/me_2021-0913-requestAnimationFrame_lucky.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0913-requestAnimationFrame_lucky320",
+    visit:"https://ig-kim2511.github.io/me_2021-0913-requestAnimationFrame_lucky320/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "Constructor,class",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "--",
+    imgSrc: "./img/projects/me_2021-0914-Constructor_class_240-js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0914-Constructor_class_240-js-folder",
+    visit:"https://ig-kim2511.github.io/me_2021-0914-Constructor_class_240-js-folder/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "input-form--js",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "css",
+    imgSrc: "./img/projects/small-input-form--js.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0131-input-form--js",
+    visit:"https://ig-kim2511.github.io/me_2022-0131-input-form--js/",    
+  },  
+  {
+    id: 613,
+    type:"--",
+    title: "slideshow",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "slideshow,autoplay,setInterval",
+    imgSrc: "./img/projects/small-slideShow.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0204-slideShow_autoplay_setInterval_clearInterval-js-folder",
+    visit:"https://ig-kim2511.github.io/me_2022-0204-slideShow_autoplay_setInterval_clearInterval-js-folder/",    
+  }, 
+  {
+    id: 614,
+    type:"small",
+    title: "fetch-axios datas",
+
+    skills: ["fa-js","fa-react"],
+    explain: "fetch-axios datas",
+    imgSrc: "./img/projects/small-me_2021-0412-fetch-axios-typingGame-Scalper.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-0412-fetch-axios-typingGame-Scalper",
+    visit:"https://ig-kim2511.github.io/me_2021-0412-fetch-axios-typingGame-Scalper/",    
+  }, 
+  {
+    id: 614,
+    type:"small",
+    title: "search filter (jQuery, bootstrap)",
+
+    skills: ["fa-js","fa-bootstrap"],
+    explain: "fetch-axios datas",
+    imgSrc: "./img/projects/small-filter_search 2022-0222-165457.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0222-SearchFilter-js",
+    visit:"https://ig-kim2511.github.io/me_2022-0222-SearchFilter-js/",    
+  }, 
+
+];
+
+
+const Data_sheet_clone = [
+  {
+    id: 203,
+    type:"js-web",
+    title: "Costco_self_checkout",
+
+    skills: ["fa-js","fa-css3"],
+    explain: "virtual keyboard, adding product to cart, calculate price, Vanilla javascript",
+    imgSrc: "./img/projects/me_2021-1030-Costco_self_checkout.jpg",
+    github: "https://github.com/IG-Kim2511/me_2021-1030-costco",
+    visit:"https://ig-kim2511.github.io/me_2021-1030-costco/",
+  
+  }, 
+    {
+      id: 700,
+      type:"clone",
+      title: "the road",
+
+      skills: ["fa-js","fa-css3"],
+      explain: "css skill: keyfram, input ",
+      imgSrc: "./img/projects/clone-the road.jpg",
+      github: "https://github.com/IG-Kim2511/the-road-0808-p",
+      visit:"https://ig-kim2511.github.io/the-road-0808-p/",    
+    },  
+    {
+      id: 701,
+      type:"clone",
+      title: "sign in",
+      skills: ["fa-js","fa-css3"],
+      explain: "css skill:navbar",
+      imgSrc: "./img/projects/clone-Sign-In-Sign-Up-Form-HCJ.jpg",
+      github: "https://github.com/IG-Kim2511/Sign-In-Sign-Up-Form-HCJ-2021-0209-P",
+      visit:"https://ig-kim2511.github.io/Sign-In-Sign-Up-Form-HCJ-2021-0209-P/",    
+    },  
+    {
+      id: 702,
+      type:"clone",
+      title: "cars",
+      skills: ["fa-js","fa-css3"],
+      explain: "css skill:input",
+      imgSrc: "./img/projects/clone-0206-cars.jpg",
+      github: "https://github.com/IG-Kim2511/Cars-HCJ-2021-0206-P",
+      visit:"https://ig-kim2511.github.io/Cars-HCJ-2021-0206-P/",    
+    },  
+ 
+    {
+      id: 704,
+      type:" ",
+      title: " Virtual Keyboard full version ",
+  
+      skills: ["fa-js","fa-css3"],
+      explain: "Virtual Keyboard for number",
+      imgSrc: "./img/projects/clone Virtual Keyboard full version.jpg",
+      github: "https://github.com/IG-Kim2511/me_2021-1001_keyboard_full",
+      visit:"https://ig-kim2511.github.io/me_2021-1001_keyboard_full/",    
+    }, 
+
+];
+
+const Data_sheet_small_react =[
+  {
+    id: 704,
+    type:" ",
+    title: "breakingbad_react_api",
+
+    skills: ["fa-react"],
+    explain: "fetch data api, react",
+    imgSrc: "./img/projects/clone-2021-0308_breakingbad_react_api.jpg",
+    github: "https://github.com/IG-Kim2511/c2021-0308_breakingbad_react_api",
+    visit:"https://ig-kim2511.github.io/c2021-0308_breakingbad_react_api_build/",    
+  }, 
+  
+  {
+    id: 401,
+    title: "Restaurant web",
+    skills: ["fa-react","fa-css3"],
+    explain: "useRef, taking data with Map Loop",
+    imgSrc: "./img/projects/me_2022-0209-restaurant-react.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0209_restaurant-react-",
+    visit:"https://ig-kim2511.github.io/me_2022-0209_restaurant-react-build-/",  
+  },
+  {
+    id: 401,
+    title: "message_window_of_action-react",
+    skills: ["fa-react","fa-css3"],
+    explain: "show the message of action, creating Btns by clicking btn, send input message to window, Dark Theme mode",
+    imgSrc: "./img/projects/react-me_2022-0322-message_window_of_action-react.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0322-message_window_of_action-react",
+    visit:"https://ig-kim2511.github.io/me_2022-0322-message_window_of_action-react-build/",  
+  },
+  {
+    id: 401,
+    title: "별코딩 me_2022-0329-starcoding10-usestate",
+    skills: ["fa-react"],
+    explain: "별코딩 usestate",
+    imgSrc: "./img/projects/react-small-me_2022-0329-starcoding10-usestate.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0329-starcoding10-usestate",
+    visit:"https://ig-kim2511.github.io/-me_2022-0329-starcoding10-usestate-build/",  
+  },
+  {
+    id: 401,
+    title: "별코딩 use_reducer",
+    skills: ["fa-react"],
+    explain: "별코딩 use_reducer",
+    imgSrc: "./img/projects/react-small-me_2022-0327-starcoding80-reducer.jpg",
+    github: "https://github.com/IG-Kim2511/star_coding-80-use_reducer-2022",
+    visit:"https://ig-kim2511.github.io/react-small-me_2022-0327-starcoding80-reducer-build/",  
+  },
+  {
+    id: 401,
+    title: "별코딩 use_effect",
+    skills: ["fa-react"],
+    explain: "input에 넣은 value...p태그에 띄우기, e.target.value, {showTimer && <Timer/>},  <button onClick={()=>{ setShowTimer(!showTimer)}}>toggle timer</button>",
+    imgSrc: "./img/projects/react-small-me_2022-0401-starcoding10-use_effect.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0401-starcoding10-use_effect",
+    visit:"https://ig-kim2511.github.io/me_2022-0401-starcoding10-use_effect-build/",  
+  },
+  {
+    id: 401,
+    title: "별코딩 use_context, Dark mode",
+    skills: ["fa-react"],
+    explain: " 🦄css에 ternary operator 넣기    isDark가     true이면? black    false이면? lightgray,    클릭하면  setIsDark(!isDark);실행   클릭할때마다 isDark <--> !isDark 바뀜 ",
+    imgSrc: "./img/projects/react-small-me_2022-0402-starcoding50-useContext.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0402-starcoding50-use_context",
+    visit:"https://ig-kim2511.github.io/me_2022-0402-starcoding50-use_context-build/",  
+  },
+  {
+    id: 401,
+    title: "별코딩 use_memo, object_data_type-2022",
+    skills: ["fa-react"],
+    explain: " object_reference_data_type",
+    imgSrc: "./img/projects/react-small-me_2022-0406-starcoding60-usememo.jpg",
+    github: "https://github.com/IG-Kim2511/me_2022-0402-starcoding50-use_context",
+    visit:"https://ig-kim2511.github.io/me_2022-0402-starcoding50-use_context-build/",  
+  },
+  {
+    id: 401,
+    title: "react-zustand",
+    skills: ["fa-react"],
+    explain: "zustand",
+    imgSrc: "./img/projects/react-small-me_2022-0328-zustand.jpg",
+    github: "https://github.com/IG-Kim2511/codingapple_2022-0328_zustand-react",
+    visit:"",  
+  },
+
+
+];
+
